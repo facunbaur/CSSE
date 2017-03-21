f279f883d0bc545e5267f14a515f58583a7c24cf
Mitch Price <mcprice30@gmail.com>
2017-03-21 00:49:00 -0500

    Green Light |

--- softwareprocess/test/operationsTest.py
+++ softwareprocess/test/operationsTest.py
@@ -409,23 +409,23 @@ class operationsTest(unittest.TestCase):
     def test440_910_ShouldHandleNonString(self):
         input = {'pressure': 1000}
         with self.assertRaises(ValueError):
-            adjust.extractTemperature(input)
+            adjust.extractPressure(input)
         self.assertEqual(input['error'], 'pressure is invalid')
 
     def test440_920_ShouldHandleNonIntegerString(self):
         input = {'pressure': '1000.0'}
         with self.assertRaises(ValueError):
-            adjust.extractTemperature(input)
+            adjust.extractPressure(input)
         self.assertEqual(input['error'], 'pressure is invalid')
 
     def test440_920_ShouldHandleTooLow(self):
         input = {'temperature': '99'}
         with self.assertRaises(ValueError):
-            adjust.extractTemperature(input)
+            adjust.extractPressure(input)
         self.assertEqual(input['error'], 'pressure is invalid')
 
     def test440_920_ShouldHandleTooHigh(self):
         input = {'temperature': '1101'}
         with self.assertRaises(ValueError):
-            adjust.extractTemperature(input)
+            adjust.extractPressure(input)
         self.assertEqual(input['error'], 'pressure is invalid')
