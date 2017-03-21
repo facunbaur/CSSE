429d37261cad22cf660940a0830d5eb996b3f367
Mitch Price <mcprice30@gmail.com>
2017-03-20 22:50:42 -0500

    Red Light |

--- softwareprocess/test/operationsTest.py
+++ softwareprocess/test/operationsTest.py
@@ -26,3 +26,26 @@ class operationsTest(unittest.TestCase):
         sighting['extra'] = 'ignore me'
         actual = correct.correct(sighting)
         self.assertEqual(actual, sighting, 'Should return unmodified')
+
+    # 200 locate
+    #   Desired level of confidence:    boundary value analysis
+    #   Input-output Analysis:
+    #       inputs:     sighting: dictionary containing 'op': 'locate', validated.
+    #       outputs:    sighting passed, unchanged.
+    #   Happy Path
+    #       dictionary containing only op
+    #       dictionary containing extra values
+    #
+    # Happy Path
+    def test200_010_ShouldPassPlainDictionary(self):
+        sighting = dict()
+        sighting['op'] = 'locate'
+        actual = correct.correct(sighting)
+        self.assertEqual(actual, sighting, 'Should return unmodified')
+
+    def test200_020_ShouldPassDictionaryWithAddedEntry(self):
+        sighting = dict()
+        sighting['op'] = 'locate'
+        sighting['extra'] = 'ignore me'
+        actual = correct.correct(sighting)
+        self.assertEqual(actual, sighting, 'Should return unmodified')
