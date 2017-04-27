f2e79c8709ed5158aa5d2dab010f9dbd803fc99a
Mitch Price <mcprice30@gmail.com>
2017-04-27 17:14:13 -0500

    Red Light |

--- softwareprocess/test/correctUnitTest.py
+++ softwareprocess/test/correctUnitTest.py
@@ -8,3 +8,13 @@ class correctUnitTest(unittest.TestCase):
         expected = 45
         actual = correct.extractMeasurement(test_input, 'lat', -90, 90)
         self.assertAlmostEqual(actual, expected, 3, 'Should extract latitude')
+
+    def test_010_090_handleMissingLatitude(self):
+        test_input = {}
+        expected = {'error': 'missing mandatory field lat'}
+        try:
+            actual = correct.extractMeasurement(test_input, 'lat', -90, 90)
+        except ValueError:
+            self.assertEqual(test_input, expected, 'Should have error')
+            return
+        self.assertTrue(False, 'Did not raise error')
