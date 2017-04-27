89fb915ca39b82737b5def56df38b460a6b5e30b
Mitch Price <mcprice30@gmail.com>
2017-04-27 17:18:54 -0500

    Red Light |

--- softwareprocess/test/correctUnitTest.py
+++ softwareprocess/test/correctUnitTest.py
@@ -38,3 +38,44 @@ class correctUnitTest(unittest.TestCase):
             self.assertEqual(test_input, expected, 'Should have error')
             return
         self.assertTrue(False, 'Did not raise error')
+
+
+
+    # LONGITUDE TESTS
+
+    def test_010_010_shouldExtractLongitude(self):
+        test_input = {'long': '45d0.0'}
+        expected = 45
+        actual = correct.extractMeasurement(test_input, 'long', 0, 360)
+        self.assertAlmostEqual(actual, expected, 3, 'Should extract longitude')
+
+    def test_010_090_handleMissingLongitude(self):
+        test_input = {}
+        expected = {'error': 'missing mandatory field long'}
+        try:
+            actual = correct.extractMeasurement(test_input, 'long', 0, 360)
+        except ValueError:
+            self.assertEqual(test_input, expected, 'Should have error')
+            return
+        self.assertTrue(False, 'Did not raise error')
+
+    def test_010_091_handleLowLongitude(self):
+        test_input = {'long': '-0d1.0'}
+        expected = {'long': '-0d1.0', 'error': 'long is invalid'}
+        try:
+            actual = correct.extractMeasurement(test_input, 'long', 0, 360)
+        except ValueError:
+            self.assertEqual(test_input, expected, 'Should have error')
+            return
+        self.assertTrue(False, 'Did not raise error')
+
+    def test_010_091_handleHighLongitude(self):
+        test_input = {'long': '360d1.0'}
+        expected = {'long': '360d1.0', 'error': 'long is invalid'}
+        try:
+            actual = correct.extractMeasurement(test_input, 'long', 0, 360)
+        except ValueError:
+            self.assertEqual(test_input, expected, 'Should have error')
+            return
+        self.assertTrue(False, 'Did not raise error')
+
