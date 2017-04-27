70e14ebc231ec0f48b8019977071a62ee0f146d6
Mitch Price <mcprice30@gmail.com>
2017-04-27 17:59:05 -0500

    Green Light |

--- softwareprocess/operations/correct.py
+++ softwareprocess/operations/correct.py
@@ -64,9 +64,10 @@ def extractMeasurement(sighting, name, lowBound, highBound):
 def calculateCorrectedDistance(lat, assumedLat, altitude, lon, assumedLon):
     intermediate = calculateIntermediateDistance(lat, assumedLat, lon, assumedLon)
     correctedAltitude = math.asin(intermediate)
-    dist = math.degrees(altitude - correctedAltitude) * 60
+    print correctedAltitude
+    print math.degrees(math.radians(altitude) - correctedAltitude)
+    dist = math.degrees(math.radians(altitude) - correctedAltitude) * 60
     return round(dist)
-    pass
 
 def calculateCorrectedAzimuth():
     pass
